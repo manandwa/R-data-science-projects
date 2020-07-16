f309cd27737f30eb8f074aff8561afc863815817
Mobin <manandwa@iupui.edu>
2020-07-16 11:34:59 -0500

    Add files via upload
    Script to determine NA values in original data

---
+++ Predicting Condominium Sale Prices/brooklyn.R
@@ -0,0 +1,19 @@
+library(readxl)
+library(readr)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+
+# Load data
+brooklyn <- read_excel("rollingsales_brooklyn.xls", skip = 4)
+
+test <- brooklyn %>% filter(`BUILDING CLASS AT TIME OF SALE` == "R4") %>% head()
+
+test_overall <- brooklyn %>% filter(`BUILDING CLASS AT TIME OF SALE` == "R4")
+unique(test_overall$`BUILDING CLASS AT TIME OF SALE`)
+
+gsf <- unique(test_overall$`GROSS SQUARE FEET`)
+gsf_no_na <- brooklyn %>% filter(`GROSS SQUARE FEET` != "NA")
+unique(gsf_no_na$`BUILDING CLASS AT TIME OF SALE`)
